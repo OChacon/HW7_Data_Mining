e899eb0d1fcad847f8cfbb6ece14d298896bd165
Oscar Chacon <orc2815@rit.edu>
2020-03-27 15:17:10 -0400

    Added print statements for report questions

--- HW07_Agglomerate.py
+++ HW07_Agglomerate.py
@@ -16,36 +16,109 @@ def calculate_correlations(data_frame):
     """
     Title: calculate_correlations
     :param data_frame: the pandas data_frame that was created from parsing the csv file
-    :return: max_corr: The maximum correlation value
-    :return: max_index: The maximum correlation index
-    :return: min_corr: The minimum correlation value
-    :return: min_index: The minimum correlation index
     Description: This method calculates all the correlations with the sickness column, and picks out the item
     that has the highest correlation, and the item that has the lowest correlation.
     """
-
     data_frame = data_frame.drop(columns=['ID'])
     correlations = data_frame.corr()
+    list_of_column_names = list(correlations.columns.values)
+    # Min is rice with soda
+    # print(correlations)
+    # correlations.to_csv('correlations.csv', index=False)
 
+    # Find the highest correlation between two attributes
+    max_corr_value = -1
+    first_attr_name = ''
+    second_attr_index = 0
+    for attr in correlations:
+        index_tracker = 0
+        for corr in correlations[attr]:
+            if corr > max_corr_value and corr != 1:
+                max_corr_value = corr
+                first_attr_name = attr
+                second_attr_index = index_tracker
+            index_tracker += 1
 
-    print(correlations)
-    correlations.to_csv('correlations.csv', index=False)
-   # sick_corrs = correlations['Sickness']
-    max_corr = -1
-    min_corr = 1
-    max_index = 0
-    min_index = 0
-    index = 0
-    # for corr in sick_corrs:
-    #     if corr > max_corr and corr != 1:
-    #         max_corr = corr
-    #         max_index = index
-    #     elif corr < min_corr and corr != -1:
-    #         min_corr = corr
-    #         min_index = index
-    #     index += 1
+    second_attr_name = list_of_column_names[second_attr_index]
+    print('The highest correlation is between attributes ' + first_attr_name + ' and ' + second_attr_name +
+          ' with a value of ' + str(max_corr_value))
 
-    return max_corr, max_index, min_corr, min_index
+    # Find which attribute fish has the highest correlation with
+    fish_max_corr_value = -1
+    fish_attr_index = 0
+    index_tracker = 0
+    for corr in correlations['  Fish']:
+        if corr > fish_max_corr_value and corr != 1:
+            fish_max_corr_value = corr
+            fish_attr_index = index_tracker
+        index_tracker += 1
+
+    fish_attr_name = list_of_column_names[fish_attr_index]
+    print('Fish has the highest correlation with attribute ' + fish_attr_name + ' with a value of ' +
+          str(fish_max_corr_value))
+
+    # Find which attribute meat has the highest correlation with
+    meat_max_corr_value = -1
+    meat_attr_index = 0
+    index_tracker = 0
+    for corr in correlations['  Meat']:
+        if corr > meat_max_corr_value and corr != 1:
+            meat_max_corr_value = corr
+            meat_attr_index = index_tracker
+        index_tracker += 1
+
+    meat_attr_name = list_of_column_names[meat_attr_index]
+    print('Meat has the highest correlation with attribute ' + meat_attr_name + ' with a value of ' +
+          str(meat_max_corr_value))
+
+    # Find which attribute beans has the highest correlation with
+    beans_max_corr_value = -1
+    beans_attr_index = 0
+    index_tracker = 0
+    for corr in correlations[' Beans']:
+        if corr > beans_max_corr_value and corr != 1:
+            beans_max_corr_value = corr
+            beans_attr_index = index_tracker
+        index_tracker += 1
+
+    beans_attr_name = list_of_column_names[beans_attr_index]
+    print('Beans has the highest correlation with attribute ' + beans_attr_name + ' with a value of ' +
+          str(beans_max_corr_value))
+
+    # Find which attribute is least correlated with all other attributes
+    average_corr_values = []
+    for attr in correlations:
+        sum = 0
+        for corr in correlations[attr]:
+            sum += corr
+        average_corr_values.append(sum/len(list_of_column_names))
+
+    lowest_average_value = 1
+    lowest_average_index = 0
+    index_tracker = 0
+    for average in average_corr_values:
+        if average < lowest_average_value:
+            lowest_average_value = average
+            lowest_average_index = index_tracker
+        index_tracker += 1
+
+    lowest_average_attr_name = list_of_column_names[lowest_average_index]
+    print('The first attribute that is least correlated with all other attributes is ' + lowest_average_attr_name +
+          ' with an average correlation value of ' + str(lowest_average_value))
+
+    # Find the second attribute that is least correlated with all other attributes
+    second_lowest_average_value = 1
+    second_lowest_average_index = 0
+    index_tracker = 0
+    for average in average_corr_values:
+        if average < second_lowest_average_value and index_tracker != lowest_average_index:
+            second_lowest_average_value = average
+            second_lowest_average_index = index_tracker
+        index_tracker += 1
+
+    second_lowest_average_attr_name = list_of_column_names[second_lowest_average_index]
+    print('The second attribute that is least correlated with all other attributes is ' +
+          second_lowest_average_attr_name + ' with an average correlation value of ' + str(second_lowest_average_value))
 
 
 if __name__ == '__main__':
@@ -75,8 +148,3 @@ if __name__ == '__main__':
     pd.set_option('display.max_rows', 1000)
     pd.set_option('display.max_columns', 1000)
     calculate_correlations(data_frame)
-
-
-
-
-
